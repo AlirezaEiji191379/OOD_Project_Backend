41a4669af8912aa8fd64e520a193eba2e1f3d90e
Alireza Eiji <AlirezaEiji151379@gmail.com>
2023-06-08 22:07:53 +0330

    Add Packages with Facades (#2)
    Co-authored-by: alirezaeiji <alirezaeiji151479@gmail.com>

---
+++ OOD_Project_Backend/Content/Facade/ContentFacade.cs
@@ -0,0 +1,8 @@
+﻿using OOD_Project_Backend.Content.Facade.Abstractions;
+
+namespace OOD_Project_Backend.Content.Facade
+{
+    public class ContentFacade : IContentFacade
+    {
+    }
+}
---
+++ OOD_Project_Backend/Payment/Facade/PaymentFacade.cs
@@ -0,0 +1,8 @@
+﻿using OOD_Project_Backend.Payment.Facade.Abstractions;
+
+namespace OOD_Project_Backend.Payment.Facade
+{
+    public class PaymentFacade : IPaymentFacade
+    {
+    }
+}
