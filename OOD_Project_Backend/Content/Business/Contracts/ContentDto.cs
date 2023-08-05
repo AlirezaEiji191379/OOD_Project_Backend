5cec3798363a78acb700dbae943081543f502b8b
youngbizman <youngbizmann@gmail.com>
2023-08-06 02:18:08 +0330

    simplify content

--- OOD_Project_Backend/Content/Business/Contracts/ContentDto.cs
+++ OOD_Project_Backend/Content/Business/Contracts/ContentDto.cs
@@ -4,12 +4,5 @@ namespace OOD_Project_Backend.Content.DataAccess.Dtos;
 
 public class ContentDto
 {
-    public int ContentId { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public string Type { get; set; }
-    public string FileName { get; set; }
-    public double Price { get; set; }
-    public bool IsPremium { get; set; }
-    public string Title { get; set; }
-    public string? Description { get; set; }
+    
 }
