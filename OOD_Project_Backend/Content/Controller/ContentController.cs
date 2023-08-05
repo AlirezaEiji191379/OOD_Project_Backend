f945bae8ee56715f93ccf7b9a65217dcf0a9fb79
youngbizman <youngbizmann@gmail.com>
2023-08-06 02:33:14 +0330

    add necessary controller code

--- OOD_Project_Backend/Content/Controller/ContentController.cs
+++ OOD_Project_Backend/Content/Controller/ContentController.cs
@@ -12,5 +12,45 @@ namespace OOD_Project_Backend.Content.Controller;
 [Route("Content")]
 public class ContentController : ControllerBase
 {
- 
+    private readonly IContentService _contentService;
+
+    public ContentController(IContentService contentService)
+    {
+        
+        _contentService = contentService;
+        
+    }
+
+    [HttpPost]
+    [Route("Add")]
+    [Authorize]
+    public async Task<Response> AddContent([FromForm] ContentCreationRequest creationRequest )
+    {
+        return await _contentService.Add(creationRequest);
+    }
+
+    [HttpGet]
+    [Route("GetContentsMetaData/{channelId}")]
+    [Authorize]
+    public async Task<Response> GetChannelContentMetadata(int channelId)
+    {
+         return await _contentService.GetChannelContentsMetadata(channelId);
+    }
+
+    [HttpGet]
+    [Route("ShowContent/{contentId}")]
+    [Authorize]
+    public async Task<FileResult> ShowContent(int contentId)
+    {
+        return await _contentService.Show(contentId);
+    }
+
+    [HttpDelete]
+    [Route("RemoveContent/{contentId}")]
+    [Authorize]
+    public async Task<Response> DeleteContent(int contentId)
+    {
+        return await _contentService.Delete(contentId);
+    }
+
 }
