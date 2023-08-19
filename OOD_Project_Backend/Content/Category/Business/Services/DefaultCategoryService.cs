35c84df7b158797fc742c5ff62c7f71db3186398
youngbizman <youngbizmann@gmail.com>
2023-08-19 13:27:33 +0330

    Add create category

---
+++ OOD_Project_Backend/Content/Category/Business/Services/DefaultCategoryService.cs
@@ -0,0 +1,90 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OOD_Project_Backend.Channel.ChannelCore.Business.Contracts;
+using OOD_Project_Backend.Content.Category.Business.Contexts;
+using OOD_Project_Backend.Content.Category.Business.Contracts;
+using OOD_Project_Backend.Content.Category.DataAccess.Entities;
+using OOD_Project_Backend.Content.Category.DataAccess.Repository.Contracts;
+using OOD_Project_Backend.Content.ContentCore.DataAccess.Repositories;
+using OOD_Project_Backend.Content.ContentCore.DataAccess.Repositories.Contracts;
+using OOD_Project_Backend.Core.Context;
+using OOD_Project_Backend.User.Business.Contracts;
+
+namespace OOD_Project_Backend.Content.Category.Business.Services;
+
+public class DefaultCategoryService : ICategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IChannelFacade _channelFacade;
+    private readonly IContentMetaDataRepository _contentMetaDataRepository;
+    private readonly IUserFacade _userFacade;
+
+    public DefaultCategoryService(
+        IChannelFacade channelFacade, 
+        IUserFacade userFacade, 
+        ICategoryRepository categoryRepository, 
+        IContentMetaDataRepository contentMetaDataRepository)
+    {
+        _channelFacade = channelFacade;
+        _userFacade = userFacade;
+        _categoryRepository = categoryRepository;
+        _contentMetaDataRepository = contentMetaDataRepository;
+    }
+
+    public async Task<Response> CreateCategory(CategoryRequest request)
+    {
+        var currentUserId = _userFacade.GetCurrentUserId();
+        try
+        {
+            var isChannelAdminOrOwner = await _channelFacade.IsChannelAdminOrOwner(currentUserId, request.ChannelId);
+
+            if (!isChannelAdminOrOwner)
+                return new Response((int)HttpStatusCode.Unauthorized,
+                    new { Message = "You are not authorized to create categories!" });
+
+            var category = await _categoryRepository.GetByName(request.ChannelId, request.Title);
+
+            if (category != null)
+            {
+                return new Response((int)HttpStatusCode.BadRequest, new { Message = "Category Already exists" });
+            }
+
+            category = new CategoryEntity
+            {
+                Title = request.Title,
+                ChannelId = request.ChannelId,
+            };
+            await _categoryRepository.Create(category);
+            await _categoryRepository.SaveChangesAsync();
+
+            return new Response((int)HttpStatusCode.Created, new { Message = "Category Created Successfully" });
+        }
+        catch (Exception)
+        {
+            return new Response((int)HttpStatusCode.BadRequest, new { Message = "Something Went Wrong!" });
+
+        }
+    }
+    public async Task<Response> DeleteCategory(CategoryRequest request)
+    {
+        throw new NotImplementedException(); 
+    }
+
+    public async Task<Response> UpdateCategory(CategoryRequest request)
+    {
+        throw new NotImplementedException(); 
+
+    }
+
+    public async Task<Response> GetCategory(CategoryRequest request)
+    {
+        throw new NotImplementedException(); 
+
+    }
+
+    public async Task<Response> GetCategories(CategoryRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ OOD_Project_Backend/Content/Category/Controller/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOD_Project_Backend.Content.Category.Business.Contexts;
+using OOD_Project_Backend.Content.Category.Business.Contracts;
+using OOD_Project_Backend.Core.Common.Authentication;
+using OOD_Project_Backend.Core.Context;
+
+namespace OOD_Project_Backend.Content.Category.Controller;
+
+[ApiController]
+[Route("Category")]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpPost]
+    [Route("Add")]
+    [Authorize]
+    public async Task<Response> Add([FromBody] CategoryRequest categoryRequest)
+    {
+        return await _categoryService.CreateCategory(categoryRequest);
+    }
+    
+}
